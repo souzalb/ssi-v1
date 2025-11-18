d75e69a4f65aa7d7a4d7ff2b38cb685a621a8b61
sn1082087 <lincoln.souza@docente.senai.br>
2025-11-17 21:54:57 -0300

    style: update padding login form

--- src/app/login/login-form.tsx
+++ src/app/login/login-form.tsx
@@ -114,7 +114,7 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="relative z-10 px-6 pb-8">
+        <CardContent className="relative z-10 px-8 pb-8">
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
             <FieldGroup>
               {/* Campo de Email */}
